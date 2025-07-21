0fbf0bd053ae272686361e0d639c74b2aaea82ca
Eudes Junior <dev.eudesjr@outlook.com>
2025-07-21 17:17:42 -0300

    refactor: folder restructuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { VStack } from "@chakra-ui/react";
-import AboutUs from "./components/about-us";
 import Footer from "./components/footer";
 import Header from "./components/header";
-import Presentation from "./components/presentation";
-import ServicesSection from "./components/services-section";
-import Testimonials from "./components/testimonials";
-import UrgentOfferSection from "./components/urgent-offer-section";
+import AboutUs from "./sections/about-us";
+import Presentation from "./sections/presentation-section";
+import ServicesSection from "./sections/services-section";
+import Testimonials from "./sections/testimonials";
+import UrgentOfferSection from "./sections/urgent-offer-section";
 
 export default function App() {
   return (
